952d1d0006d0f4cae9f93ae44ffe9dcc5b476d44
Preethi Pattabiraman <preetiauguststar@gmail.com>
2021-01-04 21:58:10 +0530

    Express HTTP Server Commit 2

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const http = require("http");
-
+const morgan = require("morgan");
 const host = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.end("<html><head><title>Hello!</title></head><body><h1>Hello World from Express!</h1></body></html");
